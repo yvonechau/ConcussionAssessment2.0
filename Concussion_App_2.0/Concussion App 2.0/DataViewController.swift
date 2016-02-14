c509fde98abe28e00661f736929862ea15e0e590
kvnf10 <kvnf10@Kevin-Fus-MacBook-Pro.local>
2016-02-14 13:10:17 -0800

    pushing XCode workspace

---
+++ Concussion_App_2.0/Concussion App 2.0/DataViewController.swift
@@ -0,0 +1,34 @@
+//
+//  DataViewController.swift
+//  Concussion App 2.0
+//
+//  Created by Kevin Fu on 2/14/16.
+//  Copyright © 2016 Kevin Fu. All rights reserved.
+//
+
+import UIKit
+
+class DataViewController: UIViewController {
+
+    @IBOutlet weak var dataLabel: UILabel!
+    var dataObject: String = ""
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        self.dataLabel!.text = dataObject
+    }
+
+
+}
+
