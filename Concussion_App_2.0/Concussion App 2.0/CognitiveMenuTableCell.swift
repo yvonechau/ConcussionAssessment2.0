1ebff61bad6fd303222e3fc7e8f731386e762287
Philson Wong <pncwong@ucdavis.edu>
2016-02-16 13:09:20 -0800

    Added TableViews into CognitiveView, need to figure out margin issues

---
+++ Concussion_App_2.0/Concussion App 2.0/CognitiveMenuTableCell.swift
@@ -0,0 +1,24 @@
+//
+//  CognitiveMenu.swift
+//  Concussion App 2.0
+//
+//  Created by Philson Wong on 2/16/16.
+//  Copyright © 2016 Kevin Fu. All rights reserved.
+//
+
+import UIKit
+
+class CognitiveMenu: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
