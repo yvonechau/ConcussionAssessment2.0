0f7b6eb05a7dd78092de93f17d0a342c03497dd8
ytchau <ytchau@ucdavis.edu>
2016-02-15 23:21:10 -0800

    Navigation Bar working for Glasgow and Symptom without storyboard

--- Concussion_App_2.0/Concussion App 2.0/RootViewController.swift
+++ Concussion_App_2.0/Concussion App 2.0/RootViewController.swift
@@ -15,6 +15,9 @@ class RootViewController: UIViewController, UIPageViewControllerDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        self.navigationItem.title = "Main Screen"
+        
         // Do any additional setup after loading the view, typically from a nib.
         // Configure the page view controller and add it as a child view controller.
         self.pageViewController = UIPageViewController(transitionStyle: .PageCurl, navigationOrientation: .Horizontal, options: nil)
