bf6a67a44323b21586b69cff447c5802b1fba26a
Philson Wong <pncwong@ucdavis.edu>
2016-02-16 17:52:50 -0800

    Added ViewControllers for Number/Orientation Tests.

---
+++ Concussion_App_2.0/Concussion App 2.0/CognitiveTableViewController.swift
@@ -0,0 +1,13 @@
+//
+//  CognitiveTableViewController.swift
+//  Concussion App 2.0
+//
+//  Created by Philson Wong on 2/16/16.
+//  Copyright © 2016 Kevin Fu. All rights reserved.
+//
+
+import Cocoa
+
+class CognitiveTableViewController: UITableViewController {
+
+}
---
+++ Concussion_App_2.0/Concussion App 2.0/OrientationTestViewController.swift
@@ -0,0 +1,13 @@
+//
+//  OrientationTestViewController.swift
+//  Concussion App 2.0
+//
+//  Created by Philson Wong on 2/16/16.
+//  Copyright © 2016 Kevin Fu. All rights reserved.
+//
+
+import Cocoa
+
+class OrientationTestViewController: UIViewController {
+
+}
