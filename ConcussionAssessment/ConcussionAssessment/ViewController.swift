47d9a8281735aba520a4564503e9a5097b0013d6
Philson Wong <pncwong@ucdavis.edu>
2016-02-18 13:55:38 -0800

    Created new Xcode project for ConcussionAssessment, deleted storyboards

---
+++ ConcussionAssessment/ConcussionAssessment/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ConcussionAssessment
+//
+//  Created by Philson Wong on 2/18/16.
+//  Copyright © 2016 PYKS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
