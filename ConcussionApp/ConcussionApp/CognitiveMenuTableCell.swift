294632fec894731c1b6e77801cebd0d811817363
Philson Wong <pncwong@ucdavis.edu>
2016-02-18 12:36:37 -0800

    Renamed Concussion_App_2.0 to ConcussionApp

---
+++ ConcussionApp/ConcussionApp/CognitiveMenuTableCell.swift
@@ -0,0 +1,27 @@
+//
+//  CognitiveMenu.swift
+//  Concussion App 2.0
+//
+//  Created by Philson Wong on 2/16/16.
+//  Copyright © 2016 Kevin Fu. All rights reserved.
+//
+
+import UIKit
+
+class CognitiveMenuTableCell: UITableViewCell {
+    // MARK: Properties
+    @IBOutlet weak var itemLabel: UILabel!
+    
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
