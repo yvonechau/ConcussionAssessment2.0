5afc2ef0d0bf50a54cd0bfd7f598de58811ec8d9
Philson Wong <pncwong@ucdavis.edu>
2016-02-23 12:15:22 -0800

    No working TableViewCells

--- ConcussionAssessment/ConcussionAssessment/AppDelegate.swift
+++ ConcussionAssessment/ConcussionAssessment/AppDelegate.swift
@@ -17,7 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
         
-        let splashScreenController = SplashScreenController(style: UITableViewStyle.Grouped)
+        let splashScreenController = TestMenuTableViewController()
         let navController = UINavigationController(rootViewController: splashScreenController)
         
         self.window = UIWindow(frame: UIScreen.mainScreen().bounds)
