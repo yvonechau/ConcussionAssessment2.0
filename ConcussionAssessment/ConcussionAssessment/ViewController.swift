ed60228936d2d6215200158fee0c7537e54223cc
ytchau <ytchau@ucdavis.edu>
2016-02-22 18:03:14 -0800

    Created Splash Screen

--- ConcussionAssessment/ConcussionAssessment/ViewController.swift
+++ ConcussionAssessment/ConcussionAssessment/ViewController.swift
@@ -6,6 +6,8 @@
 //  Copyright © 2016 PYKS. All rights reserved.
 //
 
+// ROOT VIEW CONTROLLER ??
+
 import UIKit
 
 class ViewController: UIViewController {
@@ -13,6 +15,21 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        /*
+        UIButton *button = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+        [self.view addSubview:button];
+        */
+        print("meepmoop");
+        
+        let button   = UIButton(type: UIButtonType.System) as UIButton
+        button.frame = CGRectMake(100, 100, 100, 50)
+        button.backgroundColor = UIColor.greenColor()
+        button.setTitle("Test Button", forState: UIControlState.Normal)
+        button.addTarget(self, action: "buttonAction:", forControlEvents: UIControlEvents.TouchUpInside)
+        
+        self.view.addSubview(button)
+
     }
 
     override func didReceiveMemoryWarning() {
