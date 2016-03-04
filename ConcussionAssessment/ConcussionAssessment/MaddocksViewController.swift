6720a8bc36c73c45e721eab41724d0739bfc0398
kvnf10 <kvnf10@campus-037-129.ucdavis.edu>
2016-03-04 15:35:25 -0800

    changed layout now have custom uitablecell

--- ConcussionAssessment/ConcussionAssessment/MaddocksViewController.swift
+++ ConcussionAssessment/ConcussionAssessment/MaddocksViewController.swift
@@ -6,20 +6,36 @@
 //  Copyright © 2016 PYKS. All rights reserved.
 //
 
+//Things to fix:
+//- Way to go to next test
+//- Tally Scores out of 5 after going to next test
+//- A checker to see if all answers have been filled out if proceeding to next test
+//- Fix Dimmensions to fit all ios devices
+//- UI Design
+
+
 import UIKit
 
 class MaddocksViewController: UITableViewController
 {
-  let QuestionArray = ["At what venue are we today?", "Which half is it now?", "Who scored last in this match?", "What did you play last week?", "Did your team win the last game?"]
-  var answer: UILabel!
-  var cell: UITableViewCell!
-  
-  var num = 1
+  var maddocks: [Maddocks] = []
+  let questionArray = ["At what venue are we today?", "Which half is it now?", "Who scored last in this match?", "What did you play last week?", "Did your team win the last game?"]
   
   override func viewDidLoad()
   {
     super.viewDidLoad()
     self.title = "Maddocks Test"
+    
+    for question in questionArray{
+      let maddock = Maddocks(q: question)
+      maddocks.append(maddock)
+    }
+    
+    tableView = UITableView(frame: view.bounds, style: .Grouped)
+    tableView.delegate = self
+    tableView.dataSource = self
+    tableView.separatorStyle = .None
+    tableView.registerClass(MaddocksCell.self, forCellReuseIdentifier: NSStringFromClass(MaddocksCell))
   }
   
   override func didReceiveMemoryWarning()
@@ -29,78 +45,112 @@ class MaddocksViewController: UITableViewController
   }
   
   override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    return QuestionArray.count
+    return questionArray.count
   }
   
   override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
     
-    //Table
-    cell = UITableViewCell(style: UITableViewCellStyle.Subtitle, reuseIdentifier: "MenuCell")
-    cell.textLabel?.text = QuestionArray[indexPath.row]
+    let cell = tableView.dequeueReusableCellWithIdentifier( NSStringFromClass(MaddocksCell), forIndexPath: indexPath) as! MaddocksCell
+    cell.maddocks = maddocks[indexPath.row]
     self.tableView.rowHeight = 120.0
-
-    //Correct Button
-    let correct = UIButton(type: UIButtonType.System) as UIButton
-    correct.frame = CGRectMake(10, 80, 100, 40)
-    correct.setTitle("Correct", forState: UIControlState.Normal)
-    correct.backgroundColor = UIColor.cyanColor()
-    correct.addTarget(self, action: "buttonAction:", forControlEvents: UIControlEvents.TouchUpInside)
-    correct.tag = indexPath.row
-    cell.addSubview(correct)
-
-    
-    //Incorrect Button
-    let incorrect = UIButton(type: UIButtonType.System) as UIButton
-    incorrect.frame = CGRectMake(150, 80, 100, 40)
-    incorrect.setTitle("Incorrect", forState: UIControlState.Normal)
-    incorrect.backgroundColor = UIColor.cyanColor()
-    incorrect.addTarget(self, action: "buttonAction:", forControlEvents: UIControlEvents.TouchUpInside)
-    incorrect.tag = indexPath.row
-    cell.addSubview(incorrect)
-
-    
-    //Answer Text Label
-    answer = UILabel(frame: CGRectMake(290, 80, 100, 40))
-    answer.textAlignment = NSTextAlignment.Center
-    answer.text = "Test"
-    answer.backgroundColor = UIColor.redColor()
-    answer.textColor = UIColor.whiteColor()
-    cell.addSubview(answer)
-
-    
-    
-//    let answer = UIButton(frame: CGRectMake(10, 80, 300, 10))
-//    answer.addTarget(self, action: "sliderValueChanged:", forControlEvents: UIControlEvents.ValueChanged)// set the target to respond to changes.
-//    answer.userInteractionEnabled = true
-//    answer.tag = indexPath.row// save the indexPath.row
-//    cell.addSubview(answer)
-    
-//    var value = slider.value
-//    let label = UILabel(frame: CGRectMake(320, 80, 100, 20))
-//    if round(answer.value) == 1
-//    {
-//      value = true
-//    }
-//    else
-//    {
-//      value = false
-//    }
-//    label.text = "\(value)"
-//    cell.addSubview(label)
-    
     return cell
+  }
+  
+  //  override func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+  //    return 70
+  //  }
+  
+  
+}
+
+class Maddocks
+{
+  var question: String?
+  var answer: String?
+  init(q: String)
+  {
+    self.question = q
+    self.answer = " "
+  }
+}
+
+class MaddocksCell: UITableViewCell
+{
+  //let padding: CGFloat = 5
+  var questionLabel: UILabel!
+  var correctButton: UIButton!
+  var incorrectButton: UIButton!
+  var answerLabel: UILabel!
+  
+  var maddocks: Maddocks?
+    {
+    didSet
+    {
+      questionLabel.backgroundColor = UIColor.orangeColor()
+      questionLabel.text = maddocks!.question
+      
+      correctButton.backgroundColor = UIColor.cyanColor()
+      correctButton.setTitle("Correct", forState: UIControlState.Normal)
+      
+      incorrectButton.backgroundColor = UIColor.cyanColor()
+      incorrectButton.setTitle("Incorrect", forState: UIControlState.Normal)
+      
+      answerLabel.backgroundColor = UIColor.redColor()
+      setNeedsLayout()
+      
+    }
+  }
+  
+  
+  override init(style: UITableViewCellStyle, reuseIdentifier: String?)
+  {
+    super.init(style: style, reuseIdentifier: reuseIdentifier)
+    backgroundColor = UIColor.whiteColor()
+    selectionStyle = .None
+    
+    questionLabel = UILabel(frame: CGRectZero)
+    questionLabel.textAlignment = .Left
+    questionLabel.textColor = UIColor.blackColor()
+    contentView.addSubview(questionLabel)
+    
+    
+    correctButton = UIButton(frame: CGRectZero)
+    correctButton.addTarget(self, action: "buttonAction:", forControlEvents: UIControlEvents.TouchUpInside)
+    contentView.addSubview(correctButton)
+    
+    incorrectButton = UIButton(frame:CGRectZero)
+    incorrectButton.addTarget(self, action: "buttonAction:", forControlEvents: UIControlEvents.TouchUpInside)
+    contentView.addSubview(incorrectButton)
+    
+    answerLabel = UILabel(frame: CGRectZero)
+    answerLabel.textAlignment = .Center
+    answerLabel.textColor = UIColor.whiteColor()
+    contentView.addSubview(answerLabel)
+    
+  }
+  
+  required init(coder aDecoder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+  
+  override func prepareForReuse() {
+    super.prepareForReuse()
+    
   }
   
   func buttonAction(sender:UIButton!)
   {
-    print("\(sender.currentTitle!) tapped in row \(sender.tag)")
-    var indexPath: NSIndexPath = NSIndexPath(forRow: sender.tag, inSection: 0)
-    var cell = self.tableView(tableView, cellForRowAtIndexPath:indexPath)
-    //cell.answer.text = ""
-    self.answer.text = "\((num++))"
-  }
-  //table iew global
-  // make custom table view cell
+    print("\(sender.currentTitle!) tapped")
+    answerLabel.text = "\(sender.currentTitle!)"
 
+  }
   
+  
+  override func layoutSubviews() {
+    super.layoutSubviews()
+    questionLabel.frame = CGRectMake(0, 0, frame.width, 40)
+    correctButton.frame = CGRectMake(10, 80, 100, 40)
+    incorrectButton.frame = CGRectMake(150, 80, 100, 40)
+    answerLabel.frame = CGRectMake(290, 80, 100, 40)
+  }
 }
