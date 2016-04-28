42ba321ad9ec48bd311f2754ce48f734c7c867ef
Seanna Vien <seannavien@Seannas-Mac.local>
2016-04-28 16:22:12 -0700

    neck should be overriding certain functions

--- ConcussionAssessment/ConcussionAssessment/NeckExaminationView.swift
+++ ConcussionAssessment/ConcussionAssessment/NeckExaminationView.swift
@@ -14,223 +14,26 @@
 import UIKit
 
 
-class NeckExamViewController: UIViewController, UIPageViewControllerDataSource
+class NeckExamViewController: TablePageViewController
 {
-  
-  var pageViewController: UIPageViewController?
-  var testName: String
-  var pageTitles : Array<String>
-  var labelArray : Array<Array<String>>
-  var currentIndex : Int = 0
-  var limitIndex: Int = 0
-  var rowSelected: NSNumber?
-  var totalRows: Int = 0
-  var donePressed: Bool = false
-  var instructions: String
-  var next: UIViewController?
-  var original: UIViewController?
-  var startingViewController : NeckExamView?
-  var numTrials : [Int]?
-  var singlePage: BooleanType
-  
-  var numPages: Int
-  var numSelected: NSNumber
-  var currScore: NSNumber
-  
-  init(pageTitles : Array<String>, labelArray: Array<Array<String>>, testName : String, instructionPage : NeckExamView?, instructions: String, next: TablePageViewController?, original: UIViewController?, numTrials: [Int]?, singlePage: BooleanType)
-  {
-    self.pageTitles = pageTitles
-    self.labelArray = labelArray
-    self.testName = testName
-    self.startingViewController = instructionPage
-    self.instructions = instructions
-    self.next = next
-    self.original = original!
-    self.numTrials = numTrials
-    self.singlePage = singlePage
-    self.numPages = 0
-    self.numSelected = 0
-    self.currScore = 0
-    super.init(nibName:nil, bundle:nil)
-  }
-  
-  required init?(coder aDecoder: NSCoder) {
-    fatalError("init(coder:) has not been implemented")
-  }
-  
-  func setNextTest(next: TablePageViewController?)
-  {
-    self.next = next
-  }
-  
-  func buttonPressed(sender: UIButton)
-  {
-    let alertView = UIAlertController(title: "Instructions", message: self.instructions, preferredStyle: UIAlertControllerStyle.Alert)
-    alertView.addAction(UIAlertAction(title: "Ok", style: .Default, handler: {
-      action in
-      switch action.style
-      {
-      case .Default:
-        print("default")
-      case .Cancel:
-        print("cancel")
-      case .Destructive:
-        print("destructive")
-      }
-      
-    }))
-    presentViewController(alertView, animated: true, completion: nil)
-    
-  }
-  
-  
-  override func viewDidLoad()
-  {
-    super.viewDidLoad()
-    pageViewController = UIPageViewController(transitionStyle: .Scroll, navigationOrientation: .Horizontal, options: nil)
-    pageViewController!.dataSource = self
-    
-    if(self.startingViewController == nil) // not instantiated so it has no instruction page
-    {
-      self.startingViewController = viewControllerAtIndex(0)!
-    }
-    
-    let viewControllers = [self.startingViewController!]
-    pageViewController!.setViewControllers(viewControllers, direction: .Forward, animated: false, completion: nil)
-    pageViewController!.view.frame = CGRectMake(0, 0, view.frame.size.width, view.frame.size.height);
-    
-    
-    addChildViewController(pageViewController!)
-    view.addSubview(pageViewController!.view)
-    pageViewController!.didMoveToParentViewController(self)
-    
-    
-    /***** TITLE SETTINGS ****
-     *********************************/
-    self.navigationItem.title = self.testName
-    
-    
-    /***** RIGHT NAV BAR BUTTONS ****
-     *********************************/
-    let infobutton = UIButton(type: UIButtonType.InfoDark)
-    
-    infobutton.addTarget(self, action: #selector(TablePageViewController.buttonPressed(_:)), forControlEvents: UIControlEvents.TouchUpInside)
-    let infoModalButton : UIBarButtonItem? = UIBarButtonItem(customView: infobutton)
-    
-    
-    if(self.singlePage)
-    {
-      let doneButton = UIBarButtonItem(barButtonSystemItem: .Done, target: self, action: #selector(TablePageViewController.doneButtonPressed(_:)))
-      self.navigationItem.rightBarButtonItems = [doneButton, infoModalButton!]
-    }
-    else
-    {
-      self.navigationItem.rightBarButtonItems = [infoModalButton!]
-    }
-    
-  }
-  
-  override func didReceiveMemoryWarning()
-  {
-    super.didReceiveMemoryWarning()
-  }
-  
-  func pageViewController(pageViewController: UIPageViewController, viewControllerBeforeViewController viewController: UIViewController) -> UIViewController?
-  {
-    var index = self.currentIndex
-    
-    if(index == 0) || (index == NSNotFound)
-    {
-      return nil
-    }
-    index -= 1
-    
-    return viewControllerAtIndex(index)
-  }
-  
-  func pageViewController(pageViewController: UIPageViewController, viewControllerAfterViewController viewController: UIViewController) -> UIViewController?
-  {
-    var index = self.currentIndex
-    if index == NSNotFound
-    {
-      return nil
-    }
-    
-    index += 1
-    currentIndex = index
-    limitIndex = index
-    
-    if(index == self.pageTitles.count)
-    {
-      return nil
-    }
-    currentIndex = index
-    
-    return viewControllerAtIndex(index)
-  }
-  
-  func viewControllerAtIndex(index: Int) ->NeckExamView?
-  {
-    if self.pageTitles.count == 0 || index >= self.pageTitles.count || index < limitIndex
-    {
-      return nil
-    }
-    
-    let pageContentViewController = NeckExamView(nvc: self)
-    pageContentViewController.titleText = pageTitles[index]
-    
-    return pageContentViewController
-  }
-  
-  
-  func presentationCountForPageViewController(pageViewController: UIPageViewController) -> Int
-  {
-    self.numPages = self.pageTitles.count
-    
-    return self.numPages
-    
-  }
-  
-  func presentationIndexForPageViewController(pageViewController: UIPageViewController) -> Int
-  {
-    return self.currentIndex
-  }
   
 }
 
-class NeckExamView: UITableViewController, UIPickerViewDataSource, UIPickerViewDelegate
+class NeckExamView: TablePageView, UIPickerViewDataSource, UIPickerViewDelegate
 {
-  var titleText : String = ""
-  var rowSel : NSNumber = 0
-  
-  var totalRowsSelected : Int = 0
-  var checked : [Bool]
-  weak var nvc : NeckExamViewController?
-  let LabelArray : Array<Array<String>>
   
   var textField: UITextField
   var pickerView: UIPickerView
   
-  init(nvc : NeckExamViewController)
-  {
-    self.nvc = nvc
-    
-    self.LabelArray = nvc.labelArray
-    self.checked = [Bool](count: self.nvc!.pageTitles.count, repeatedValue: false)
-    super.init(style: UITableViewStyle.Grouped)
-  }
   
-  required init?(coder aDecoder: NSCoder) {
-    fatalError("init(coder:) has not been implemented")
+  required init?(coder aDecoder: NSCoder)
+  {
+    <#code#>
   }
   
   override func viewDidLoad()
   {
     super.viewDidLoad()
-    
-    self.tableView.contentInset = UIEdgeInsetsMake(120.0, 0, -120.0, 0)
-    self.tableView.separatorStyle = UITableViewCellSeparatorStyle.SingleLine
-    self.tableView.rowHeight = 50.0
     
     self.pickerView.dataSource = self
     self.pickerView.delegate = self
