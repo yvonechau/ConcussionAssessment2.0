62d435dd700ae126939cae9cebc312f11dc2a15f
Christine Franks <clfranks@ucdavis.edu>
2016-04-28 17:12:17 -0700

    Corrected math for player profiles' collection view layout.
    Based on a specified (constant) number of columns and inner spacing, recalculate the proper size and origin of each element of the layout (I assume they are square).

--- ConcussionAssessment/ConcussionAssessment/ProfileCollectionViewLayout.swift
+++ ConcussionAssessment/ConcussionAssessment/ProfileCollectionViewLayout.swift
@@ -20,22 +20,35 @@ class ProfileCollectionViewLayout: UICollectionViewFlowLayout {
     
     override func layoutAttributesForElementsInRect(rect: CGRect) -> [UICollectionViewLayoutAttributes]? {
         var attr = super.layoutAttributesForElementsInRect(rect)!
-        for i in 1 ..< attr.count {
-            let currentLayoutAttributes: UICollectionViewLayoutAttributes = attr[i] 
-            let prevLayoutAttributes: UICollectionViewLayoutAttributes = attr[i - 1]
-            let maximumSpacing: CGFloat = 5
-            let originX: CGFloat = CGRectGetMaxX(prevLayoutAttributes.frame)
-            let originY: CGFloat = CGRectGetMaxY(prevLayoutAttributes.frame)
-            if originX + maximumSpacing + currentLayoutAttributes.frame.size.width < self.collectionViewContentSize().width {
-                var frame: CGRect = currentLayoutAttributes.frame
-                frame.origin.x = originX + maximumSpacing
-                currentLayoutAttributes.frame = frame
+        // need to determine the frame / size for each square on the grid; 
+        let numberOfColumns = 3
+        let innerSpacing: CGFloat = 2
+        let squareDims = (self.collectionViewContentSize().width - CGFloat(numberOfColumns + 1) * innerSpacing) / CGFloat(numberOfColumns)
+        
+        var currentLeftOffset : CGFloat = innerSpacing
+        var currentTopOffset : CGFloat = innerSpacing
+        for i in 0 ..< attr.count {
+            let currentLayoutAttributes: UICollectionViewLayoutAttributes = attr[i]
+            
+            // update our current origin
+            if (currentLayoutAttributes.indexPath.row % numberOfColumns == 0 && currentLayoutAttributes.indexPath.row != 0) {
+                currentLeftOffset = innerSpacing
+                currentTopOffset = currentTopOffset + innerSpacing + squareDims
+            } else if (currentLayoutAttributes.indexPath.row != 0){
+                currentLeftOffset = currentLeftOffset + innerSpacing + squareDims
             }
-            if originY + maximumSpacing + currentLayoutAttributes.frame.size.height < self.collectionViewContentSize().height {
-                var frame: CGRect = currentLayoutAttributes.frame
-                frame.origin.y = originY + maximumSpacing
-                currentLayoutAttributes.frame = frame
-            }
+            
+            // adjust the size to the new dimensions
+            var layoutAttrSize : CGSize = currentLayoutAttributes.size
+            layoutAttrSize.height = CGFloat(squareDims)
+            layoutAttrSize.width = CGFloat(squareDims)
+            currentLayoutAttributes.size = layoutAttrSize
+            
+            // adjust the frame to the new location
+            var frame: CGRect = currentLayoutAttributes.frame
+            frame.origin.x = currentLeftOffset
+            frame.origin.y = currentTopOffset
+            currentLayoutAttributes.frame = frame
         }
         return attr
     }
